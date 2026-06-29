257a9d02b2a5fbfca3d96cc4480713c029e99607
matitrova <matitrova13@gmail.com>
2026-06-29 07:16:53 -0300

    wip

--- IspbossTest/AboNuevo.py
+++ IspbossTest/AboNuevo.py
@@ -1,6 +1,6 @@
 class AboNuevo:
     def __init__(self, page):
-        self.page = page 
+        self.page = page
         self.numero_calle_input = page.locator("#ctl00_ctl00_b_b_ucAlta_txtInstalacionNumero")
         self.calle_seleccionada = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlInstalacionCalle-container")
         self.zona_seleccionada = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlInstalacionZona-container")
@@ -10,31 +10,36 @@ class AboNuevo:
         self.abo_alta_apellido = page.locator("#ctl00_ctl00_b_b_ucAlta_txtApellido")
         self.abo_alta_categoria = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container")
         self.abo_alta_metod_envio_comp = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlComprobanteMetodoEnvio-container")
-        self.cod_area = page.get_by_placeholder("Cód.Area")
-        self.abo_alta_telefono = page.get_by_placeholder("Número")
+        self.cod_area = page.locator('input[name="ctl00$ctl00$b$b$ucAlta$txtCelular$ctl02"]')
+        self.abo_alta_celular = page.locator('input[name="ctl00$ctl00$b$b$ucAlta$txtCelular$ctl03"]')
         self.abo_alta_email = page.locator("#ctl00_ctl00_b_b_ucAlta_txtEmail")
+        self.locator_Next_paso3 = page.locator("#ctl00_ctl00_b_b_ucAlta_btnGuardar2")
+        self.selec_forma_pago_alta_abo = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlFormaPago-container")
+        self.locator_Next_paso4 = page.locator("#ctl00_ctl00_b_b_ucAlta_btnGuardar3")
 
-    def completar_nro_calle(self, numero):
-        self.numero_calle_input.fill(numero)
-    
-    def obtener_nro_calle(self):
-        input_value = self.numero_calle_input.input_value()
-        return input_value
-    
+    # ---------- PASO 1: Domicilio de Instalación ----------
+
     def seleccionar_cualquier_calle(self):
         self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlInstalacionCalle-container").click()
         self.page.locator(".select2-results__option").first.click()
-    
+
     def obtener_calle_seleccionada(self):
-        calle_seleccionada = self.calle_seleccionada.inner_text()
-        return calle_seleccionada
-    
+        return self.calle_seleccionada.inner_text()
+
+    def completar_nro_calle(self, numero):
+        self.numero_calle_input.fill(numero)
+        self.numero_calle_input.blur()    # ← saca el foco, dispara el cálculo de zona
+
+    def obtener_nro_calle(self):
+        return self.numero_calle_input.input_value()
+
     def obtener_zona_seleccionada(self):
-        zona_seleccionada = self.zona_seleccionada.inner_text()
-        return zona_seleccionada
-    
+        return self.zona_seleccionada.inner_text()
+
     def click_next_paso2(self):
         self.locator_Next_paso2.click()
+
+    # ---------- PASO 2: Datos Personales ----------
 
     def completar_documento(self, documento):
         self.abo_alta_Documento.fill(documento)
@@ -53,36 +58,47 @@ class AboNuevo:
 
     def obtener_apellido(self):
         return self.abo_alta_apellido.input_value()
-
-
+    
     def seleccionar_cualquier_categoria(self):
         self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container").click()
         self.page.locator(".select2-results__option").first.click()
-    
+
     def obtener_categoria_seleccionada(self):
-        categoria_seleccionada = self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container").inner_text()
-        return categoria_seleccionada
-    
+        return self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container").inner_text()
+
     def seleccionar_metodo_envio_comprobante(self):
         self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlComprobanteMetodoEnvio-container").click()
-        self.page.locator(".select2-results__option", has_Text="Email").click()
+        self.page.locator(".select2-results__option", has_text="Email").click()
 
     def completar_cod_area(self, cod_area):
-        self.page.get_by_placeholder("Cód.Area").fill(cod_area)
+        self.cod_area.fill(cod_area)
 
     def obtener_cod_area(self):
-        return self.page.get_by_placeholder("Cód.Area").input_value()
-    
+        return self.cod_area.input_value()
+
     def completar_telefono(self, numero):
-        self.abo_alta_telefono.fill(numero)
-    
+        self.abo_alta_celular.fill(numero)
+
     def obtener_telefono(self):
-        return self.abo_alta_telefono.input_value()
-    
+        return self.abo_alta_celular.input_value()
+
     def completar_email(self, email):
         self.abo_alta_email.fill(email)
 
     def obtener_email(self):
         return self.abo_alta_email.input_value()
-    
 
+    def click_next_paso3(self):
+        self.locator_Next_paso3.click()
+
+    # ---------- PASO 3: Datos de Facturación ----------
+
+    def seleccionar_forma_pago(self):
+        self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlFormaPago-container").click()
+        self.page.locator(".select2-results__option", has_text="Contado").click()
+
+    def obtener_forma_pago(self):
+        return self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlFormaPago-container").inner_text()
+
+    def click_next_paso4(self):
+        self.locator_Next_paso4.click()
--- IspbossTest/test_alta_abonado.py
+++ IspbossTest/test_alta_abonado.py
@@ -1,14 +1,15 @@
-from IspbossLoginPage import LoginIspBoss 
-from MenuPage import MenuPage 
+from IspbossLoginPage import LoginIspBoss
+from MenuPage import MenuPage
 from AboNuevo import AboNuevo
+from playwright.sync_api import expect
 
 
-def test_alta_abonado(page): 
-    login_page = LoginIspBoss(page) 
-    menu_page = MenuPage(page) 
+def test_alta_abonado(page):
+    login_page = LoginIspBoss(page)
+    menu_page = MenuPage(page)
 
-    login_page.navigate() 
-    login_page.login("mati", "1234") 
+    login_page.navigate()
+    login_page.login("mati", "1234")
     menu_page.navigate()
     menu_page.click_abonados_menu()
     locator_paso1 = page.get_by_text("Paso 1: Domicilio de Instalación")
@@ -16,48 +17,60 @@ def test_alta_abonado(page):
     assert locator_paso1.is_visible()
 
     abo_nuevo = AboNuevo(page)
+
+    # Primero la calle (tu pedido: es más rápido que arrancar por el número)
+    abo_nuevo.seleccionar_cualquier_calle()
+    calle_seleccionada = abo_nuevo.obtener_calle_seleccionada()
+    assert calle_seleccionada != "Seleccione"
+
+    # Después el número
     abo_nuevo.completar_nro_calle("123")
     numero_calle_input = abo_nuevo.obtener_nro_calle()
     assert numero_calle_input == "123"
 
-    selec_calle = abo_nuevo.seleccionar_cualquier_calle()
-    calle_seleccionada = abo_nuevo.obtener_calle_seleccionada()
-    assert calle_seleccionada != "Seleccione"
-
-    abo_nuevo.zona_seleccionada.wait_for()
+    # Zona se autocompleta (tarda 1-2 seg): esperamos a que deje de decir "Seleccione"
+    expect(abo_nuevo.zona_seleccionada).not_to_have_text("Seleccione")
     zona_seleccionada = abo_nuevo.obtener_zona_seleccionada()
     assert zona_seleccionada != "Seleccione"
 
-    next_paso2 = abo_nuevo.click_next_paso2()
+    abo_nuevo.click_next_paso2()
     locator_paso2 = page.get_by_text("Paso 2: Datos Personales")
     locator_paso2.wait_for()
-    assert page.get_by_text("Paso 2: Datos Personales").is_visible()
+    assert locator_paso2.is_visible()
 
-    completar_documento = abo_nuevo.completar_documento("12345678")
-    documento_completado = abo_nuevo.obtener_documento()
-    assert documento_completado == "12345678"
+    abo_nuevo.completar_documento("12345678")
+    assert abo_nuevo.obtener_documento() == "12345678"
 
-    completar_nombre = abo_nuevo.completar_nombre("Juan")
-    nombre = abo_nuevo.obtener_nombre()
-    assert nombre == "Juan"
+    abo_nuevo.completar_nombre("Juan")
+    assert abo_nuevo.obtener_nombre() == "Juan"
 
-    completar_apellido = abo_nuevo.completar_apellido("Gomez")
-    apellido = abo_nuevo.obtener_apellido()
-    assert apellido == "Gomez"
+    abo_nuevo.completar_apellido("Gomez")
+    assert abo_nuevo.obtener_apellido() == "Gomez"
 
-    selec_categoria = abo_nuevo.seleccionar_cualquier_categoria()
-    categoria_seleccionada = abo_nuevo.obtener_categoria_seleccionada()
-    assert categoria_seleccionada != "Seleccione"
+    abo_nuevo.seleccionar_cualquier_categoria()
+    assert abo_nuevo.obtener_categoria_seleccionada() != "Seleccione"
 
-    completar_cod_area = abo_nuevo.completar_cod_area("011")
-    cod_area = abo_nuevo.obtener_cod_area()
-    assert cod_area == "011"
+    abo_nuevo.seleccionar_metodo_envio_comprobante()
 
-    completar_telefono = abo_nuevo.completar_telefono("12345678")
-    telefono = abo_nuevo.obtener_telefono()
-    assert telefono == "12345678"
+    abo_nuevo.completar_cod_area("11")
+    assert abo_nuevo.obtener_cod_area() == "11"
 
-    completar_email = abo_nuevo.completar_email("matitrova13@gmail.com")
-    email = abo_nuevo.obtener_email()
-    assert email == "matitrova13@gmail.com"
+    abo_nuevo.completar_telefono("12345678")
+    assert abo_nuevo.obtener_telefono() == "12345678"
 
+    abo_nuevo.completar_email("matitrova13@gmail.com")
+    assert abo_nuevo.obtener_email() == "matitrova13@gmail.com"
+
+    abo_nuevo.click_next_paso3()
+    locator_paso3 = page.get_by_text("Paso 3: Datos de Facturación")
+    locator_paso3.wait_for()
+    assert locator_paso3.is_visible()
+
+    abo_nuevo.seleccionar_forma_pago()
+    forma_pago = abo_nuevo.obtener_forma_pago()
+    assert forma_pago != "Seleccione"
+
+    abo_nuevo.click_next_paso4()
+    locator_paso4 = page.get_by_text("Paso 4: Confirmación")
+    locator_paso4.wait_for()
+    assert locator_paso4.is_visible()
