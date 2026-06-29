dca8a7eae34dfd7be130728aa2df7492378f6290
matitrova <matitrova13@gmail.com>
2026-06-29 01:59:19 -0300

    wip

--- IspbossTest/AboNuevo.py
+++ IspbossTest/AboNuevo.py
@@ -5,6 +5,14 @@ class AboNuevo:
         self.calle_seleccionada = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlInstalacionCalle-container")
         self.zona_seleccionada = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlInstalacionZona-container")
         self.locator_Next_paso2 = page.locator("#ctl00_ctl00_b_b_ucAlta_btnGuardar1")
+        self.abo_alta_Documento = page.locator("#ctl00_ctl00_b_b_ucAlta_txtDocumentoNumero")
+        self.abo_alta_Nombre = page.locator("#ctl00_ctl00_b_b_ucAlta_txtNombre")
+        self.abo_alta_apellido = page.locator("#ctl00_ctl00_b_b_ucAlta_txtApellido")
+        self.abo_alta_categoria = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container")
+        self.abo_alta_metod_envio_comp = page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlComprobanteMetodoEnvio-container")
+        self.cod_area = page.get_by_placeholder("Cód.Area")
+        self.abo_alta_telefono = page.get_by_placeholder("Número")
+        self.abo_alta_email = page.locator("#ctl00_ctl00_b_b_ucAlta_txtEmail")
 
     def completar_nro_calle(self, numero):
         self.numero_calle_input.fill(numero)
@@ -27,5 +35,54 @@ class AboNuevo:
     
     def click_next_paso2(self):
         self.locator_Next_paso2.click()
+
+    def completar_documento(self, documento):
+        self.abo_alta_Documento.fill(documento)
+
+    def obtener_documento(self):
+        return self.abo_alta_Documento.input_value()
+
+    def completar_nombre(self, nombre):
+        self.abo_alta_Nombre.fill(nombre)
+
+    def obtener_nombre(self):
+        return self.abo_alta_Nombre.input_value()
+
+    def completar_apellido(self, apellido):
+        self.abo_alta_apellido.fill(apellido)
+
+    def obtener_apellido(self):
+        return self.abo_alta_apellido.input_value()
+
+
+    def seleccionar_cualquier_categoria(self):
+        self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container").click()
+        self.page.locator(".select2-results__option").first.click()
     
+    def obtener_categoria_seleccionada(self):
+        categoria_seleccionada = self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlCategoria-container").inner_text()
+        return categoria_seleccionada
     
+    def seleccionar_metodo_envio_comprobante(self):
+        self.page.locator("#select2-ctl00_ctl00_b_b_ucAlta_ddlComprobanteMetodoEnvio-container").click()
+        self.page.locator(".select2-results__option", has_Text="Email").click()
+
+    def completar_cod_area(self, cod_area):
+        self.page.get_by_placeholder("Cód.Area").fill(cod_area)
+
+    def obtener_cod_area(self):
+        return self.page.get_by_placeholder("Cód.Area").input_value()
+    
+    def completar_telefono(self, numero):
+        self.abo_alta_telefono.fill(numero)
+    
+    def obtener_telefono(self):
+        return self.abo_alta_telefono.input_value()
+    
+    def completar_email(self, email):
+        self.abo_alta_email.fill(email)
+
+    def obtener_email(self):
+        return self.abo_alta_email.input_value()
+    
+
--- IspbossTest/test_alta_abonado.py
+++ IspbossTest/test_alta_abonado.py
@@ -32,3 +32,32 @@ def test_alta_abonado(page):
     locator_paso2 = page.get_by_text("Paso 2: Datos Personales")
     locator_paso2.wait_for()
     assert page.get_by_text("Paso 2: Datos Personales").is_visible()
+
+    completar_documento = abo_nuevo.completar_documento("12345678")
+    documento_completado = abo_nuevo.obtener_documento()
+    assert documento_completado == "12345678"
+
+    completar_nombre = abo_nuevo.completar_nombre("Juan")
+    nombre = abo_nuevo.obtener_nombre()
+    assert nombre == "Juan"
+
+    completar_apellido = abo_nuevo.completar_apellido("Gomez")
+    apellido = abo_nuevo.obtener_apellido()
+    assert apellido == "Gomez"
+
+    selec_categoria = abo_nuevo.seleccionar_cualquier_categoria()
+    categoria_seleccionada = abo_nuevo.obtener_categoria_seleccionada()
+    assert categoria_seleccionada != "Seleccione"
+
+    completar_cod_area = abo_nuevo.completar_cod_area("011")
+    cod_area = abo_nuevo.obtener_cod_area()
+    assert cod_area == "011"
+
+    completar_telefono = abo_nuevo.completar_telefono("12345678")
+    telefono = abo_nuevo.obtener_telefono()
+    assert telefono == "12345678"
+
+    completar_email = abo_nuevo.completar_email("matitrova13@gmail.com")
+    email = abo_nuevo.obtener_email()
+    assert email == "matitrova13@gmail.com"
+
